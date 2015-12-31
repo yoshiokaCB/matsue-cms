89c39b5c101fd1770a2ee1e9994fb7769e7fdb11
吉岡隆行 <yoshi-air@2013.local>
2016-01-01 00:43:34 +0900

    ckeditor設定終了

--- app/assets/javascripts/admin_page.js
+++ app/assets/javascripts/admin_page.js
@@ -2,3 +2,20 @@ var date_format = {'format': 'yyyy-MM-dd hh:mm:ss UTC' };
 $(function(){
   $('.datetimepicker').datetimepicker( date_format );
 });
+
+CKEDITOR.config.toolbar = [
+    ['Source','-','Save','NewPage','Preview','-','Templates']
+    ,['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print','SpellChecker']
+    ,['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat']
+    ,['Form','Checkbox','Radio','TextField','Textarea','Select','Button','ImageButton','HiddenField']
+    ,'/'
+    ,['Bold','Italic','Underline','Strike','-','Subscript','Superscript']
+    ,['NumberedList','BulletedList','-','Outdent','Indent','Blockquote']
+    ,['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock']
+    ,['Link','Unlink','Anchor']
+    ,['Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak']
+    ,'/'
+    ,['Styles','Format','Font','FontSize']
+    ,['TextColor','BGColor']
+    ,['ShowBlocks']
+];
